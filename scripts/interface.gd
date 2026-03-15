59ad784bd2c125ad9c20645ace72973d59b45774
Angel Bitsov <angel.bitsov@gmail.com>
2026-03-14 21:25:08 -0500

    Initial Stuffs

---
+++ scripts/interface.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var menu_bar: Label = %MenuBar
+@onready var ouput_lbl: RichTextLabel = %OuputLabel
+@onready var command_line: TextEdit = %CommandLine
+
+func _ready() -> void:
+	command_line.text_changed.connect(_on_command_entered)
+
+func _on_command_entered() -> void:
+	var text: String = command_line.text
+	if not text.contains("\n"): return
+	
+	var command: String = text.split("\n")[0]
+	
+	parse_command(command)
+	command_line.clear()
+
+func parse_command(command_string: String) -> void:
+	var split_command = command_string.split(" ")
+	var command: String = split_command[0]
+	split_command.remove_at(0)
+	var arguments: PackedStringArray = split_command
+	
+	print("Commands: \t%s" % command)
+	print("Arguments: \t%s" % arguments)
+	
